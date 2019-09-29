6df71e1151d72aba75a23eae953a5043ccd8d59d
Mukund MC <mukundmv08@gmail.com>
2019-09-29 19:31:33 +0530

    test

--- lock.py
+++ lock.py
@@ -1,1 +1,13 @@
-print ('test123')
+from time import sleep
+
+a = 'python123#'
+b = input("password")
+if a == b:
+    st = "your computer is open"
+    print(st)
+    sleep(10.0)
+
+else:
+        cb = "oops!!!! try again"
+        print(cb)
+        sleep(10.0)
